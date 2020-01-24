673414bc477443743deaec0edb096d92daf82de4
Angga Nugraha <angganugrahasone@gmail.com>
2020-01-24 17:56:16 +0700

    Initial Commit

---
+++ hello.py
@@ -0,0 +1,22 @@
+# Clear Terminal Screen
+import os
+os.system('clear')
+
+###############################################
+# Strings
+# Numbers
+# Lists
+# Tuples
+# Dictionaries
+# Boolean
+###############################################
+first_name = "Angga Nugraha"
+age = 34
+fav_pizza = {
+    "John": "Pepperoni",
+    "Bob" : "Mushroom",
+    "Mary" : "Cheese"
+    }
+name = True
+
+print (name) 
